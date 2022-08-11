2b16a7f68ba5ba392104d8e69b3140b79195ba20
seongwon <syanggu@naver.com>
2022-08-11 18:29:11 +0900

    위젯 분할 toppart 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -17,31 +17,44 @@ class _HomeScreenState extends State<HomeScreen> {
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Text('Start', style: TextStyle(
-                color: Colors.lightGreen,
-                fontFamily: 'pasisienne',
-                fontSize: 80
-              ),),
-              Text('2022 08 08', style: TextStyle(
-                fontFamily: 'sunflower',
-                fontSize: 30
-              ),),
-              IconButton(
-                  iconSize: 50,
-                  onPressed: (){},
-                  icon: Icon(
-                    Icons.favorite,
-                    color: Colors.pinkAccent,
-              )),
-              Text('D+1', style: TextStyle(
-                fontFamily: 'sunflower',
-                fontWeight: FontWeight.w700,
-                fontSize: 60
-              ),)
+              _TopPart(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Start', style: TextStyle(
+            color: Colors.lightGreen,
+            fontFamily: 'pasisienne',
+            fontSize: 80
+        ),),
+        Text('2022 08 08', style: TextStyle(
+            fontFamily: 'sunflower',
+            fontSize: 30
+        ),),
+        IconButton(
+            iconSize: 50,
+            onPressed: (){},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.pinkAccent,
+            )),
+        Text('D+1', style: TextStyle(
+            fontFamily: 'sunflower',
+            fontWeight: FontWeight.w700,
+            fontSize: 60
+        ),)
+      ],
     );
   }
 }
